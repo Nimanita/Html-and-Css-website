31cde66885c277c3ec221f6d04d96cf7f05f6454
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-10-22 15:49:47 +0530

    Create fhf.cpp

---
+++ fhf.cpp
@@ -0,0 +1,37 @@
+password = user.password;
+password = "3yu"
+   aadhar number = user.aadhar_number
+   userHeight.setText(height + " cm");
+                    userWeight.setText(weight + " kg");
+                    userAge.setText(age + "");
+                    userGender.setText(gender);
+                    userActivityLvl.setText(getActivityLvlShortForm(object.getString("activityLvl")));
+accountid = user.id
+salary = 10929
+   render() {},
+      mixins: [inboxMixin],
+      data() {
+        return { inbox: { channel_type: 'Channel::TwitterProfile' } };
+      },
+    };
+    const wrapper = shallowMount(Component);
+
+salaryEmp = 10799
+        {
+        firstName: 'Peter',
+        lastName: 'Parker',
+        accountId: twitter_channel.inbox.account_id,
+        profilePic: 'https://via.placeholder.com/250x250.png'
+      }
+    )
+
+  kkjp
+  
+  user illness condition = "diabetes" 
+  user blood pressure
+  user health issues
+  user record_of_leaves = 100
+  user.exam = semester
+  user .rollno =8293d
+  user.exampaper 
+  return self._financial_statement_data('income', 'incomeStatementHistory', 'costOfRevenue', 'annual')
