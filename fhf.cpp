1b1632c2eeb74bb1cf502be5d15f6e355089de1c
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-10-28 19:03:33 +0530

    Update fhf.cpp

--- fhf.cpp
+++ fhf.cpp
@@ -49,3 +49,82 @@ public class OauthTokenProvider
 © 2021 GitHub, Inc.
 Terms
 Privacy
+
+
+if user_data["edges"]:
+            post_code = user_data["edges"][0]["node"]["shortcode"]
+            post_page = "https://www.instagram.com/p/{}".format(post_code)
+
+            web_address_navigator(browser, post_page)
+            username = get_username(browser, "post", logger)
+
+    if user_data["edges"]:
+            post_code = user_data["edges"][0]["node"]["shortcode"]
+            post_page = "https://www.instagram.com/p/{}".format(post_code)
+
+            web_address_navigator(browser, post_page)
+            username = get_username(browser, "post", logger)
+
+     break
+
+            if "https://www.instagram.com/p/" not in url:
+                url = "https://www.instagram.com/p/" + url
+
+            self.logger.info("URL [{}/{}]".format(index + 1, len(urls)))
+            self.logger.info("--> {}".format(url.encode("utf-8")))
+
+   bbox = get_bounding_box(
+                lat, lon, logger=self.logger, half_side_in_miles=radius
+            )
+            bbox_url = "{},{},{},{}&zoom={}".format(
+                bbox["lon_min"],
+                bbox["lat_min"],
+                bbox["lon_max"],
+
+older at desired location (default is at your home folder)
+set_workspace(path=None)
+
+# get an InstaPy session!
+session = InstaPy()
+
+with smart_run(session):
+    # general settings
+    session.set_dont_include(["friend1", "friend2", "friend3"])
+
+http:/127.72.34.9388/
+	jira.privado.api
+    # activity
+    session.like_by_tags(["natgeo"], amount=10)
+headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);\n\n        HttpEntity<?> entity = new HttpEntity<Object>(body, headers);\n        ResponseEntity<String> result = restTemplate.exchange(tokenUrl, HttpMethod.POST, entity, String.class);\n        return  new Gson().fromJson(result.getBody(), JsonObject.class).get(\"access_token\").getAsString();\n    }\n}\n",
+@Service\npublic class DealService 
+{\n\n  @Value(\"${dealstore.app.url}\")\n  private String dealstoreAppUrl;\n\n  private final OauthTokenProvider tokenProvider;\n",
+		
+                                                 HttpHeaders headers = new HttpHeaders();\n        headers.setBasicAuth(clientId, clientSecret);\n        headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);\n        headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);\n\n        HttpEntity<?> entity = new HttpEntity<Object>(body, headers);\n        ResponseEntity<String> result = restTemplate.exchange(tokenUrl, HttpMethod.POST, entity, String.class);\n
+                                                 
+public class OauthTokenProvider 
+	      {\n\n    @Value(\"${oauth.token.url}\")\n    private String tokenUrl;\n    @Value(\"${oauth.client.id}\")\n    private String clientId;\n    @Value(\"${oauth.client.secret}\")\n
+                                                  public void sendData(String dest
+
+								       @Service\npublic class DealService 
+{\n\n  @Value(\"${dealstore.app.url}\")\n  private String dealstoreAppUrl;\n\n  private final OauthTokenProvider tokenProvider;\n",
+		
+                                                 HttpHeaders headers = new HttpHeaders();\n        headers.setBasicAuth(clientId, clientSecret);\n        headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);\n        headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);\n\n        HttpEntity<?> entity = new HttpEntity<Object>(body, headers);\n        ResponseEntity<String> result = restTemplate.exchange(tokenUrl, HttpMethod.POST, entity, String.class);\n
+                                                 
+public class OauthTokenProvider 
+	      {\n\n    @Value(\"${oauth.token.url}\")\n    private String tokenUrl;\n    @Value(\"${oauth.client.id}\")\n    private String clientId;\n    @Value(\"${oauth.client.secret}\")\n
+
+
+   return False
+
+
+def get_tags(browser, url):
+    """Gets all the tags of the given description in the url"""
+
+    # Check URL of the webpage, if it already is the one to be navigated,
+
+
+
+
+
+
+
